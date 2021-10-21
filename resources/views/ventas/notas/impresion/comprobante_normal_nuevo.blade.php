559c9fd0ef642c95b2d37112496f80d1c1b00ca6
CarlosCubas16 <ccubas@unitru.edu.pe>
2021-10-20 19:46:28 -0500

    update

--- resources/views/ventas/notas/impresion/comprobante_normal_nuevo.blade.php
+++ resources/views/ventas/notas/impresion/comprobante_normal_nuevo.blade.php
@@ -188,7 +188,12 @@
         <div class="cabecera">
             <div class="logo">
                 <div class="logo-img">
+                    @if($empresa->ruta_logo)
                     <img src="{{ base_path() . '/storage/app/'.$empresa->ruta_logo }}" class="img-fluid">
+                    @else
+                    <img src="{{ public_path() . '/img/default.png' }}" class="img-fluid">
+                    @endif
+                    
                 </div>
             </div>
             <div class="empresa">
